34e15c222b3788d699ad65debe371ddd6576ce4a
haijun <haijunq@gmail.com>
2014-04-09 11:33:00 -0700

    decoration

--- gen/com/example/service/R.java
+++ gen/com/example/service/R.java
@@ -10,6 +10,17 @@ package com.example.service;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int actionbar_background=0x7f040001;
+        public static final int actionbar_text=0x7f040000;
+        public static final int button=0x7f040006;
+        public static final int button_pressed=0x7f040007;
+        public static final int button_selected=0x7f040008;
+        public static final int nav_drawer_background=0x7f040002;
+        public static final int nav_drawer_background_pressed=0x7f040003;
+        public static final int nav_drawer_counter_background=0x7f040005;
+        public static final int nav_drawer_counter_text=0x7f040004;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,31 +28,31 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int popup_btn_dismiss=0x7f080003;
-        public static final int popup_btn_open=0x7f080004;
-        public static final int popup_text=0x7f080002;
-        public static final int startBtn=0x7f080000;
-        public static final int stopBtn=0x7f080001;
+        public static final int action_settings=0x7f090005;
+        public static final int popup_btn_dismiss=0x7f090003;
+        public static final int popup_btn_open=0x7f090004;
+        public static final int popup_text=0x7f090002;
+        public static final int startBtn=0x7f090000;
+        public static final int stopBtn=0x7f090001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int popup=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -65,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/service/PopupView.java
+++ src/com/example/service/PopupView.java
@@ -2,7 +2,9 @@ package com.example.service;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -10,7 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PopupView extends LinearLayout {
-	private TextView tv;
+	private TextView tv,tl;
 	private Button btn_dismiss;
 	private Button btn_open;
 	private WindowManager wm; 
@@ -19,13 +21,33 @@ public class PopupView extends LinearLayout {
 		super(context);
 		wm = wmr;
 		this.setOrientation(1);
+		this.setBackgroundResource(R.color.nav_drawer_background);
+		this.setPadding(10, 10, 10, 10);
+		
+		tl = new TextView(context);
+		tl.setText("Carethy");
+		tl.setTextSize(36);
+		tl.setTextColor(getResources().getColor(R.color.actionbar_background));
+		tl.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
+		tl.setGravity(Gravity.LEFT);
+		this.addView(tl);
+		
+		View sp = new View(context);
+		sp.setBackgroundResource(R.color.actionbar_background);
+		sp.setLayoutParams(new LinearLayout.LayoutParams(-1, 5));
+		this.addView(sp);
 		
 		tv = new TextView(context);
 		tv.setText(str);
 		tv.setTextSize(18);
+		tv.setTextColor(getResources().getColor(R.color.actionbar_background));
 		tv.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
+		tv.setPadding(10, 10, 10, 10);
 		this.addView(tv);
 
+		LinearLayout lly = new LinearLayout(context);
+		lly.setOrientation(0);
+		
 		btn_dismiss = new Button(context);
 		btn_dismiss.setText("Dismiss");
 		btn_dismiss.setOnClickListener(new OnClickListener(){
@@ -34,7 +56,9 @@ public class PopupView extends LinearLayout {
 	        	removePopup();
 	        }
 		});
-		this.addView(btn_dismiss);
+		btn_dismiss.setLayoutParams(new LinearLayout.LayoutParams(-1, -1, (float) 1.0));
+		btn_dismiss.getBackground().setColorFilter(getResources().getColor(R.color.button), PorterDuff.Mode.MULTIPLY);
+		lly.addView(btn_dismiss);
 		
 		btn_open = new Button(context);
 		btn_open.setText("Open");
@@ -47,7 +71,12 @@ public class PopupView extends LinearLayout {
 	        	v.getContext().startActivity(intent);
 	        }
 		});
-		this.addView(btn_open);
+		btn_open.setLayoutParams(new LinearLayout.LayoutParams(-1, -1, (float) 1.0));
+		btn_open.getBackground().setColorFilter(getResources().getColor(R.color.button), PorterDuff.Mode.MULTIPLY);
+		
+		lly.addView(btn_open);
+		
+		this.addView(lly);
 
 	}
 
